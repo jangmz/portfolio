5dcf9043f58ea64c89269dd846171c929b90480f
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2024-12-03 12:18:31 +0100

    added new project

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -2,6 +2,7 @@ import styles from "./ProjectsStyles.module.css";
 import inventoryAppLogo from "../../assets/inventory-2.png";
 import onlineShopLogo from "../../assets/online-store-2.png";
 import clubhouseLogo from "../../assets/clubhouse-2.png";
+import skyvaultApp from "../../assets/sky-vault.png";
 import ProjectCard from "./ProjectCard";
 
 function Projects() {
@@ -10,8 +11,16 @@ function Projects() {
         <h1 className="sectionTitle">Projects</h1>
         <div className={styles.projectsContainer}>
             <ProjectCard 
+                src={skyvaultApp}
+                alt="SkyVault app"
+                linkCode="https://github.com/jangmz/SkyVault"
+                linkDemo="https://skyvault-production.up.railway.app"
+                title="SkyVault Storage"
+                desc="Cloud Storage"
+            />
+            <ProjectCard 
                 src={inventoryAppLogo}
-                alt="Inventory app logo"
+                alt="Inventory app"
                 linkCode="https://github.com/jangmz/school-inventory-app"
                 linkDemo="https://school-inventory-app.up.railway.app/"
                 title="School tech inventory app"
@@ -19,7 +28,7 @@ function Projects() {
             />
             <ProjectCard 
                 src={onlineShopLogo}
-                alt="Online store logo"
+                alt="Online store"
                 linkCode="https://github.com/jangmz/infinity-online-shop"
                 linkDemo="https://infinity-online-shop.netlify.app/"
                 title="Online Store"
@@ -27,7 +36,7 @@ function Projects() {
             />
             <ProjectCard 
                 src={clubhouseLogo}
-                alt="Clubhouse logo"
+                alt="Clubhouse"
                 linkCode="https://github.com/jangmz/members-only"
                 linkDemo="https://members-only-clubhouse23.up.railway.app"
                 title="Members only Clubhouse"
