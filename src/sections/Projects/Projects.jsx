b437346d2701654241a3f654bef76ab9483faed2
Jan Jankovic <131482887+jangmz@users.noreply.github.com>
2025-01-11 17:56:52 +0100

    note styling fixed

--- src/sections/Projects/Projects.jsx
+++ src/sections/Projects/Projects.jsx
@@ -10,8 +10,11 @@ function Projects() {
   return (
     <section id="projects" className={styles.container} >
         <h1 className="sectionTitle">Projects</h1>
-        <p><strong>Please Note:</strong>
-        Some apps may take up to a minute to start as they are hosted on a service that puts them to sleep during periods of inactivity. If an app doesn't work immediately, please wait a moment and try again. Thank you for your patience!</p>
+        <div className="noteParagraph">
+            <strong>Please Note:</strong>
+            Some apps may take up to a minute to start as they are hosted on a service that puts them to sleep during periods of inactivity. If an app doesn't work immediately, please wait 1 min and try again. Thank you for your patience!
+            
+        </div>
         <div className={styles.projectsContainer}>
             <ProjectCard 
                 src={blogApp}
